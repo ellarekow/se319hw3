520dd8167bf4384d43f1d08dc289238059661aae
ella <justella2001@gmail.com>
2022-02-28 12:58:15 -0600

    Input for second part

---
+++ Task2/hw3.js
@@ -0,0 +1,9 @@
+var readline = require('readline-sync');
+
+var term1 = readline.question("1st Number: ");
+var term2 = readline.question("2st Number: ");
+var term3 = readline.question("3st Number: ");
+var term4 = readline.question("4st Number: ");
+
+
+
